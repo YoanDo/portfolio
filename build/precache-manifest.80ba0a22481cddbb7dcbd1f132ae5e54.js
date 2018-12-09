8c53285fe141bd5fb9af8200f7ed19ae3efaf1a3
Y <y.n.dorny@gmail.com>
2018-12-09 16:54:18 +0100

    base

---
+++ build/precache-manifest.80ba0a22481cddbb7dcbd1f132ae5e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4718afe88eec889090d6",
+    "url": "/static/css/main.370c97d0.chunk.css"
+  },
+  {
+    "revision": "4718afe88eec889090d6",
+    "url": "/static/js/main.4718afe8.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "678d36724d105825431f6a13dffb3618",
+    "url": "/index.html"
+  }
+];
