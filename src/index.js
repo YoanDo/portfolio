280eb9d74b31c209bc16a5913e1fd892f662b264
Y <y.n.dorny@gmail.com>
2018-12-09 17:32:59 +0100

    package update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+/*
+    Author: yoando
+    Date: 2018
+    Contact: y.n.dorny@gmail.com
+    License: /
+*/
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import Index from './components/indexComponent';
+// import store from './store';
+import messagesFr from './translations/fr';
+import messagesEn from './translations/en';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const messages = {
+  fr: messagesFr,
+  en: messagesEn,
+};
+const language = navigator.language.split(/[-_]/)[0];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const css = require('./index.css');
+
+render(
+  <IntlProvider locale={language} messages={messages[language]}>
+    <Index />
+  </IntlProvider>,
+  document.getElementById('root'),
+);
