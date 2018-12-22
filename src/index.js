4abb540e598c6087025a897209b40eee752081f2
Y <y.n.dorny@gmail.com>
2018-12-23 00:54:26 +0100

    react parallax

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { IntlProvider } from 'react-intl';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import Index from './components/indexComponent';
 import messagesFr from './assets/translations/fr';
 import messagesEn from './assets/translations/en';
@@ -22,8 +23,10 @@ const language = navigator.language.split(/[-_]/)[0];
 const css = require('./main.scss');
 
 render(
-  <IntlProvider locale={language} messages={messages[language]}>
-    <Index />
-  </IntlProvider>,
+  <ParallaxProvider>
+    <IntlProvider locale={language} messages={messages[language]}>
+      <Index />
+    </IntlProvider>
+  </ParallaxProvider>,
   document.getElementById('root'),
 );
