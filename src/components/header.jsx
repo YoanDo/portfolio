7ad33741e2d1038b48081770a1297f17a16de158
Y <y.n.dorny@gmail.com>
2018-12-23 19:12:09 +0100

    header parallax effect

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Parallax } from 'react-scroll-parallax';
+
 
 export default class Header extends Component {
   constructor(props) {
@@ -7,13 +9,21 @@ export default class Header extends Component {
     };
   }
 
+
   render() {
     return (
       <div id="header">
-        <h4>Yoan Dorny</h4>
-        <h1 className="header-text slideInUp">
-          °/yo
-        </h1>
+        <h4>Yoan Dorny.</h4>
+          <Parallax
+            className="custom-class"
+            offsetXMax={100}
+            offsetXMin={-100}
+            slowerScrollRate={true}
+          >
+            <h1 className="header-text slideInUp">
+              °/yo
+            </h1>
+        </Parallax>
       </div>
     );
   }
